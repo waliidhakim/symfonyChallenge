b33f8fb9f536a439e05037066229aed2cfbdb43e
widir <waild.idir@sanoma.com>
2023-09-09 18:52:34 +0200

    Testing the new scrapper bundle

--- src/Services/ScrappingService.php
+++ src/Services/ScrappingService.php
@@ -1,8 +1,48 @@
 <?php
+//
+//namespace App\Services;
+//
+//use Goutte\Client;
+//
+//class ScrappingService
+//{
+//    private $client;
+//
+//    public function __construct()
+//    {
+//        $this->client = new Client();
+//    }
+//
+//    public function scrapeProductDetails(string $url): array
+//    {
+//        $crawler = $this->client->request('GET', $url);
+////        $name = $crawler->filter('meta[property="og:title"]')->attr('content');
+////        $price = $crawler->filter('meta[property="product:price:amount"]')->attr('content');
+////        $image = $crawler->filter('meta[property="og:image"]')->attr('content');
+//
+//        // Exemple d'extraction des informations. A adapter en fonction des sites cibles.
+//        try {
+//            $name = $crawler->filter("#productTitle")->text();
+//            $price = $crawler->filter('.a-offscreen')->text();
+//           $image = $crawler->filter('.imgTagWrapper img')->attr('src');
+//        }catch (\Exception $exc) {
+//           throw $exc;
+//        }
+//
+//
+//        return [
+//           'name' => $name,
+//            'price' => $price,
+//          'image' => $image,
+//        ];
+//    }
+//}
+
 
 namespace App\Services;
 
-use Goutte\Client;
+use Symfony\Component\BrowserKit\HttpBrowser;
+use Symfony\Component\HttpClient\HttpClient;
 
 class ScrappingService
 {
@@ -10,30 +50,26 @@ class ScrappingService
 
     public function __construct()
     {
-        $this->client = new Client();
+        $this->client = new HttpBrowser(HttpClient::create());
     }
 
     public function scrapeProductDetails(string $url): array
     {
         $crawler = $this->client->request('GET', $url);
-//        $name = $crawler->filter('meta[property="og:title"]')->attr('content');
-//        $price = $crawler->filter('meta[property="product:price:amount"]')->attr('content');
-//        $image = $crawler->filter('meta[property="og:image"]')->attr('content');
 
         // Exemple d'extraction des informations. A adapter en fonction des sites cibles.
         try {
             $name = $crawler->filter("#productTitle")->text();
             $price = $crawler->filter('.a-offscreen')->text();
-           $image = $crawler->filter('.imgTagWrapper img')->attr('src');
-        }catch (\Exception $exc) {
-           throw $exc;
+            $image = $crawler->filter('.imgTagWrapper img')->attr('src');
+        } catch (\Exception $exc) {
+            throw $exc;
         }
 
-
         return [
-           'name' => $name,
+            'name' => $name,
             'price' => $price,
-          'image' => $image,
+            'image' => $image,
         ];
     }
 }
