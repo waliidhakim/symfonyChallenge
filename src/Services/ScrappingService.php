35fa29fba7627da0ac38147ab5781f8c73cc4972
widir <waild.idir@sanoma.com>
2023-11-05 16:42:09 +0100

    scrapping url 3

--- src/Services/ScrappingService.php
+++ src/Services/ScrappingService.php
@@ -50,19 +50,35 @@ class ScrappingService
 
     public function __construct()
     {
-        $this->client = new HttpBrowser(HttpClient::create());
+        // $this->client = new HttpBrowser(HttpClient::create());
+
+        $this->client = new HttpBrowser(HttpClient::create([
+            'headers' => [
+                'User-Agent' => 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3',
+            ],
+        ]));
     }
+
+
 
     public function scrapeProductDetails(string $url): array
     {
+        sleep(rand(2, 5));
         $crawler = $this->client->request('GET', $url);
+
+        // Vérifiez si un CAPTCHA a été déclenché
+        if ($crawler->filter('title')->text() == 'Robot Check') {
+            // Gérer le CAPTCHA ou arrêter le processus ici
+            throw new \Exception('CAPTCHA déclenché - arrêt du scraping pour cette URL.');
+        }
+
         dd($crawler);
         // Exemple d'extraction des informations. A adapter en fonction des sites cibles.
         try {
             // $name = $crawler->filter("#productTitle")->text();
             $price = $crawler->filter('.a-offscreen')->text();
             $name = $crawler->filter('#productTitle')->text('', true);
-            $image = $crawler->filter('#imgTagWrapper img')->attr('src');
+            $image = $crawler->filter('.imgTagWrapper img')->attr('src');
         } catch (\Exception $exc) {
             dd($exc);
             throw $exc;
