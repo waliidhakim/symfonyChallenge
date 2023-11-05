f35fcf5af15f2070ac6f1f557b56f2f2f40cd9ec
widir <waild.idir@sanoma.com>
2023-11-05 15:54:45 +0100

    scrapping url 3

--- src/Services/ScrappingService.php
+++ src/Services/ScrappingService.php
@@ -59,17 +59,17 @@ class ScrappingService
 
         // Exemple d'extraction des informations. A adapter en fonction des sites cibles.
         try {
-            // $name = $crawler->filter("#productTitle")->text();
-            // $price = $crawler->filter('.a-offscreen')->text();
+            $name = $crawler->filter("#productTitle")->text();
+            $price = $crawler->filter('.a-offscreen')->text();
             $image = $crawler->filter('#imgTagWrapper img')->attr('src');
         } catch (\Exception $exc) {
-            // dd($exc);
+            dd($exc);
             throw $exc;
         }
 
         return [
-            // 'name' => $name,
-            // 'price' => $price,
+            'name' => $name,
+            'price' => $price,
             'image' => $image,
         ];
     }
